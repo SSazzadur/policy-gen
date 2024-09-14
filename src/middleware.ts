728454abebf039181263497f81b3a56176f38d73
Sazzadur <ssasahil000@gmail.com>
2024-09-14 15:44:09 +0530

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
-// export default clerkMiddleware((auth, request) => {
-// 	if (!isPublicRoute(request)) {
-// 		auth().protect();
-// 	}
-// });
-
-export default clerkMiddleware();
+export default clerkMiddleware((auth, request) => {
+	if (!isPublicRoute(request)) {
+		auth().protect();
+	}
+});
 
 export const config = {
 	matcher: [

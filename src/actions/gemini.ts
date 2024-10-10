d8e4a1903dbccb95d07599ddfaee31b799398c92
Sazzadur <ssasahil000@gmail.com>
2024-10-11 00:38:47 +0530

    Add sharp@0.33.5 and error handling in askGemini function.

--- src/actions/gemini.ts
+++ src/actions/gemini.ts
@@ -2,21 +2,25 @@ import { Content } from "@google/generative-ai";
 import { Message } from "@prisma/client";
 
 export const askGemini = async (text: string, messages: Message[], policyType: PolicyType) => {
-	const baseURL = process.env.NEXT_PUBLIC_APP_URL;
+	try {
+		const baseURL = process.env.NEXT_PUBLIC_APP_URL;
 
-	const url = `${baseURL}/api/generate/${policyType}`;
+		const url = `${baseURL}/api/generate/${policyType}`;
 
-	const history: Content[] = messages.map(message => ({
-		parts: [{ text: message.message }],
-		role: message.by,
-	}));
+		const history: Content[] = messages.map(message => ({
+			parts: [{ text: message.message }],
+			role: message.by,
+		}));
 
-	const res = await fetch(url, {
-		method: "POST",
-		body: JSON.stringify({ text, history }),
-	});
+		const res = await fetch(url, {
+			method: "POST",
+			body: JSON.stringify({ text, history }),
+		});
 
-	const data = await res.json();
+		const data = await res.json();
 
-	return data;
+		return data;
+	} catch (error) {
+		console.log("error", error);
+	}
 };
